db9bbf9a788d70af7bf13f0de3932a46d5116f2c
Blackpaper13 <chris321.yusti@gmail.com>
2023-05-21 23:02:02 +0700

    sampai buat QR Code dulu

---
+++ app/src/main/java/com/example/qr_code_scanner/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.qr_code_scanner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.qr_code_scanner.databinding.ActivityMainBinding;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.todoScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setPrompt("Silkan Scan QR Code");
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            String content = intentResult.getContents();
+            if (content != null) {
+                AlertDialog.Builder alertDialogBuilder =
+                        new AlertDialog.Builder(this)
+                                .setTitle("GPS is Settings")
+                                .setMessage(content);
+                alertDialogBuilder.show();
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+
+        }
+
+    }
+
+
+
+}
